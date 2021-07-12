a6a5471b3ddbe12bac0179f2c534cb64a7b7e21d
luyang93 <510426762@qq.com>
2021-07-12 18:59:51 +0800

    Re-factored test and ingredients

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -6,30 +6,14 @@ from core.models import Tag, Ingredient
 from recipe.serializers import TagSerializer, IngredientSerializer
 
 
-class TagViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
-    """Manage tags in the database"""
-    authentication_classes = (TokenAuthentication,)
-    permission_classes = (IsAuthenticated,)
-    queryset = Tag.objects.all()
-    serializer_class = TagSerializer
-
-    def get_queryset(self):
-        """
-        Return objects for the current authenticated user only
-        """
-        return self.queryset.filter(user=self.request.user).order_by('-name')
-
-
-class IngredientViewSet(mixins.ListModelMixin,
-                        mixins.CreateModelMixin,
-                        viewsets.GenericViewSet):
+class BaseRecipeAttrViewSet(mixins.ListModelMixin,
+                            mixins.CreateModelMixin,
+                            viewsets.GenericViewSet):
     """
-    Manage ingredients in the database
+    Base viewset for user owned recipe attributes
     """
     authentication_classes = (TokenAuthentication,)
     permission_classes = (IsAuthenticated,)
-    queryset = Ingredient.objects.all()
-    serializer_class = IngredientSerializer
 
     def get_queryset(self):
         """
@@ -39,6 +23,22 @@ class IngredientViewSet(mixins.ListModelMixin,
 
     def perform_create(self, serializer):
         """
-        Create a new ingredient
+        Create a new object
         """
         serializer.save(user=self.request.user)
+
+
+class TagViewSet(BaseRecipeAttrViewSet):
+    """
+    Manage tags in the database
+    """
+    queryset = Tag.objects.all()
+    serializer_class = TagSerializer
+
+
+class IngredientViewSet(BaseRecipeAttrViewSet):
+    """
+    Manage ingredients in the database
+    """
+    queryset = Ingredient.objects.all()
+    serializer_class = IngredientSerializer
